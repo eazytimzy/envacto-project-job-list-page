2edfff605e80746b753d1023851416e7c7168e63
eazytimzy <61963475+eazytimzy@users.noreply.github.com>
2020-06-22 18:36:11 +0100

    complete job list page

---
+++ main.js
@@ -0,0 +1,19 @@
+const hamburger = document.getElementById('hamburger');
+
+const menu = document.getElementById('menu')
+
+
+hamburger.addEventListener('click', () => {
+    hamburger.classList.toggle('show');   
+    menu.classList.toggle('show'); 
+    });
+
+
+
+//    const fulltime= getElementById("ft");
+//     const parttime = getElementById("pt");
+//     const intern = getElementById("it");
+
+//     function choose(){
+//  fulltime.style.color = "green"
+//     }
